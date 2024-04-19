1bf794950153001334ee7913a2fced045b3d9b65
JangYeongwoo <ywjang@digicaps.com>
2024-04-19 15:56:46 +0900

    WishWidget TabRow 추가

--- app/src/main/java/com/example/tourmanage/ui/wish/WishWidget.kt
+++ app/src/main/java/com/example/tourmanage/ui/wish/WishWidget.kt
@@ -1,9 +1,36 @@
 package com.example.tourmanage.ui.wish
 
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.example.tourmanage.R
+import com.google.accompanist.pager.ExperimentalPagerApi
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun WishWidget() {
-    Text(text = "Wish")
+    var curIndex by remember { mutableStateOf(0) }
+    val tab = listOf("1", "2", "3")
+    TabRow(
+        selectedTabIndex = curIndex,
+        contentColor = colorResource(id = R.color.black),
+        containerColor = colorResource(id = R.color.white_smoke),
+        indicator = {
+        }) {
+        tab.forEachIndexed { index, tab ->
+            Tab(
+                modifier = Modifier.height(40.dp),
+                selected = curIndex == index,
+                onClick = {
+                    curIndex = index
+                })
+            {
+                Text(text = "$tab")
+
+            }
+        }
+    }
 }
