27e51d8b62574408af1cf75758005a9ee6bbdf3d
chaewonkim <kch9870@naver.com>
2024-04-21 21:59:51 +0900

    관광지 정보 조회
    -. 관광지 조회

--- app/src/main/java/com/example/tourmanage/common/repository/ServiceAPI.kt
+++ app/src/main/java/com/example/tourmanage/common/repository/ServiceAPI.kt
@@ -78,6 +78,8 @@ interface ServiceAPI {
         @Query("MobileApp") app: String = MOBILE_APP,
         @Query("serviceKey") key: String = API_KEY,
         @Query("areaCode") areaCode: String?,
-        @Query("_type") type: String = TYPE
+        @Query("_type") type: String = TYPE,
+        @Query("contentTypeId") contentType: String = "12",
+        @Query("arrange") arrange: String? = Config.ARRANGE_TYPE.O.name,
     ): TourInfo
 }
