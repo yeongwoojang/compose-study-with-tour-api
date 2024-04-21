2d20fbe4cce262af78602e9bc10a31897597c8e2
jangyeongwoo <sbtmxhs@gmail.com>
2024-04-21 17:46:45 +0900

    - FestivalMain화면 CrossFade 배너 추가

--- app/src/main/java/com/example/tourmanage/ui/FestivalMainActivity.kt
+++ app/src/main/java/com/example/tourmanage/ui/FestivalMainActivity.kt
@@ -10,14 +10,26 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.tourmanage.common.data.server.item.FestivalItem
+import com.example.tourmanage.common.data.server.item.StayDetailItem
+import com.example.tourmanage.common.extension.intentSerializable
+import com.example.tourmanage.common.value.Config
 import com.example.tourmanage.ui.festival.FestivalBanner
 import com.example.tourmanage.ui.ui.theme.TourManageTheme
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
 class FestivalMainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val receivedData = intent.intentSerializable(Config.PASS_DATA, java.util.ArrayList::class.java)
+        var festivalItems: ArrayList<FestivalItem>? = null
+        if (receivedData is ArrayList<*>) {
+            festivalItems = receivedData as? ArrayList<FestivalItem>
+            Timber.d("festivalItems: $festivalItems")
+        }
+
         setContent {
             TourManageTheme {
                 // A surface container using the 'background' color from the theme
@@ -25,7 +37,11 @@ class FestivalMainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    FestivalBanner()
+                    if (festivalItems != null) {
+                        FestivalBanner(festivalItems = festivalItems)
+                    } else {
+                        //TODO 데이터 없는 경우
+                    }
                 }
             }
         }
