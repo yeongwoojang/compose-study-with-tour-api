bf3f9e5cacdcbf1312f6d17ecdc2a1931fe157be
jangyeongwoo <sbtmxhs@gmail.com>
2024-12-15 19:47:42 +0900

    현재 메뉴 상태 관리  - remembersavable 사용하여 상태 상세화면 이탈 시 새로고침 되지 않도록 함.

--- app/src/main/java/com/example/tourmanage/ui/stay/StayScreen.kt
+++ app/src/main/java/com/example/tourmanage/ui/stay/StayScreen.kt
@@ -3,6 +3,7 @@ package com.example.tourmanage.ui.stay
 import android.app.Activity
 import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,26 +12,26 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -39,7 +40,6 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
-import com.example.tourmanage.R
 import com.example.tourmanage.common.di.ViewModelFactoryProvider
 import com.example.tourmanage.common.extension.isEmptyString
 import com.example.tourmanage.common.extension.isSuccess
@@ -198,9 +198,34 @@ fun StayScreen(modifier: Modifier, contentId: String?, close: () -> Unit) {
             LoadingWidget()
         }
 
-//        Row(modifier = Modifier.fillMaxWidth().height(80.dp).background(Color.Yellow)) {
-//            Text("탑바")
-//        }
+        Row(
+            modifier = Modifier.fillMaxWidth().height(50.dp)
+                .background(color = if (isVisibleAppBar) Color.White else Color.Transparent),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            IconButton(
+                modifier = Modifier.size(50.dp),
+                onClick = {}
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Call,
+                    tint = if (isVisibleAppBar) Color.Black else Color.White,
+                    contentDescription = ""
+                )
+            }
+            Text("탑바")
+
+            IconButton(
+                modifier = Modifier.size(50.dp),
+                onClick = {}
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Favorite,
+                    tint = if (isVisibleAppBar) Color.Black else Color.White,
+                    contentDescription = ""
+                )
+            }
+        }
     }
 
 
